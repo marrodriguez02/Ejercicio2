b65bf5ce830bc13967710e97a7a0b59e41eae680
Desarrollador C <desarrolladorC@example.com>
2025-12-01 14:04:45 +0100

    Implementar método explora con manera A

--- PlantillaEjercicio2/main.py
+++ PlantillaEjercicio2/main.py
@@ -11,6 +11,18 @@ def cosmos(a, p, g):
             f"El {alien} del planeta {random.choce(p)} de la galaxia {random.choice(g)}"
         )
 
+def explora(a):
+    sitios_a_explorar = ["rio, lago, campo"]
+    print(f"el alien {a} explora {random.choice(sitios_a_explorar)}")
+
+def cosmos_extendido(a, p, g):
+    for alien in a:
+        explora(alien)
+        print(
+            f"El {alien} del planeta {random.choce(p)} de la galaxia {random.choice(g)}"
+        )
+    explora(a)
+
 
 """
 Rutina/Funcion principal
